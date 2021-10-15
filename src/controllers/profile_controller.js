4db3acaf6307f39c7c49cd0803cb8b22adc23ded
terminator_79 <aezz120813@gmail.com>
2021-10-16 01:05:12 +0600

    add updating user profile endpoint without updating avatar

--- src/controllers/profile_controller.js
+++ src/controllers/profile_controller.js
@@ -1,13 +1,27 @@
-const user_service = require('../services/user_service')
 const profile_service = require('../services/profile_service')
 const ProfileDto = require('../dtos/profile_dto')
-const { User } = require('../database/db')
-
+const ResumeDto = require('../dtos/resume_dto')
+const ContactDto = require('../dtos/contact_dto')
 class ProfileController {
     async edit(ctx) {
+        const user_id = ctx.request.body['user'].id
+
         const profile_dto = new ProfileDto(ctx.request.body)
-        const user_id = ctx.request.body.user.id
-        await profile_service.update_user_profile(profile_dto, user_id)
+        await profile_service.update_user_data(profile_dto, user_id)
+        
+        if(ctx.request.body['resume']) {
+            const resume_dto = new ResumeDto(ctx.request.body['resume'])
+            await profile_service.update_resume(resume_dto, user_id)
+        }
+
+        if(ctx.request.body['contact']) {
+            ctx.request.body['contact'].forEach(elem => {
+                const contact_dto = new ContactDto(elem)
+                profile_service.update_contact(contact_dto, user_id)
+            })
+            
+        }
+
         ctx.status = 200
     }
 }
--- src/routes/profile_router.js
+++ src/routes/profile_router.js
@@ -3,8 +3,6 @@ const auth_middleware = require('../middlewares/auth_middleware')
 const profile_controller = require('../controllers/profile_controller')
 
 router
-    .use(auth_middleware)
-    .get('/profile', profile_controller.getProfile)
-    .put('/profile', profile_controller.edit)
+    .put('/profile', auth_middleware, profile_controller.edit)
 
 module.exports = router
