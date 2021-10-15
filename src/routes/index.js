b5478f77bb2cab7646501cd0f5792e0fcab6e489
terminator_79 <aezz120813@gmail.com>
2021-10-16 01:04:04 +0600

    add temporary endpoint for check updated user profile

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,16 @@
 const router = require('koa-router')()
 
+const { User, Resume, Contact } = require('../database/db')
 const auth_router = require('./auth_router')
 const profile_router = require('./profile_router')
 
 router
     .use(auth_router.routes())
     .use(profile_router.routes())
+    .get('/user', async ctx => {
+        ctx.body = await User.findAll({
+            include: [ Contact, Resume]
+        })
+    })
 
 module.exports = router
