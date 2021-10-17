98728aff026ca6848e524f4bfe8b1cc184a5ec9a
terminator_79 <aezz120813@gmail.com>
2021-10-17 17:10:52 +0600

    add endpoints for test cookies

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,5 +4,11 @@ const auth_router = require('./auth_router')
 
 router
     .use(auth_router.routes())
+    .get('/', ctx => {
+        ctx.body = ctx.cookies.get('refreshToken')
+    })
+    .post('/', ctx => {
+        ctx.body = ctx.cookies.get('refreshToken')
+    })
 
 module.exports = router
