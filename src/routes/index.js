5df2ccbafbdb0cb80a535d8f647c8c0f3f51eef0
terminator_79 <aezz120813@gmail.com>
2021-10-17 17:46:59 +0600

    try to fix cookies

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,20 @@
 const router = require('koa-router')()
+const cookie = require('node-cookie')
 
 const auth_router = require('./auth_router')
 
 router
     .use(auth_router.routes())
     .get('/', ctx => {
-        ctx.body = ctx.cookies.get('refreshToken')
+        ctx.body = [ 
+            cookie.get(ctx.request, 'refreshToken'), 
+            cookie.get(ctx.request, 'user')
+        ]
+        // ctx.body = ctx.cookies.get('refreshToken')
     })
     .post('/', ctx => {
-        ctx.body = ctx.cookies.get('refreshToken')
+        cookie.create(ctx.res, 'user', ctx.request.body["user"])
+        ctx.status = 200
     })
 
 module.exports = router
