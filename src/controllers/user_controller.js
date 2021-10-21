290a72c4588be7dde470c1e1eb6f024f1a5955c4
terminator_79 <aezz120813@gmail.com>
2021-10-21 23:43:30 +0600

    add cookie module

--- src/controllers/user_controller.js
+++ src/controllers/user_controller.js
@@ -1,32 +1,34 @@
 const { JWT_REFRESH_COOKIE_MS } = require('../configs/jwt_config')
 const user_service = require('../services/user_service')
 
+const cookie = require('node-cookie')
+
 class UserController {
     async activate(ctx) {
         const { token } = ctx.params
         const userData = await user_service.activate(token)
-        ctx.cookies.set('refreshToken', userData.refreshToken, { maxAge: JWT_REFRESH_COOKIE_MS, httpOnly: true })
+        cookie.create('refreshToken', userData.refreshToken, { maxAge: JWT_REFRESH_COOKIE_MS, httpOnly: true })
         ctx.body = userData
     }
 
     async login(ctx) {
         const { email, password } = ctx.request.body
         const userData = await user_service.login(email, password)
-        ctx.cookies.set('refreshToken', userData.refreshToken, { maxAge: JWT_REFRESH_COOKIE_MS, httpOnly: true })
+        cookie.create('refreshToken', userData.refreshToken, { maxAge: JWT_REFRESH_COOKIE_MS, httpOnly: true })
         ctx.body = userData
     }
 
     async logout(ctx) {
-        const refreshToken = ctx.cookies.get('refreshToken')
+        const refreshToken = cookie.get('refreshToken')
         await user_service.logout(refreshToken)
-        ctx.cookies.set('refreshToken', '')
+        cookie.create('refreshToken', '')
         ctx.status = 200
     } 
 
     async refresh(ctx) {
-        const refreshToken = ctx.cookies.get('refreshToken')
+        const refreshToken = cookie.get('refreshToken')
         const userData = await user_service.refresh(refreshToken)
-        ctx.cookies.set('refreshToken', userData.refreshToken, { maxAge: JWT_REFRESH_COOKIE_MS, httpOnly: true })
+        cookie.create('refreshToken', userData.refreshToken, { maxAge: JWT_REFRESH_COOKIE_MS, httpOnly: true })
         ctx.body = userData
     }
 }
--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ server
         ctx.set('Access-Control-Allow-Origin', '*')
         ctx.set(
             'Access-Control-Allow-Headers',
-            'Origin, X-Requested-With, Content-Type, Accept',
+            'Origin, X-Requested-With, Content-Type, Accept', // , Access-Control-Allow-Credentials, Credentials
         )
         ctx.set('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS')
         ctx.set('Access-Control-Allow-Credentials', 'true')
@@ -28,6 +28,8 @@ server
     })
 
     .use(errorMiddleware)
+
+    // .use(cors({ credentials: true }))
 
     // parsers
     .use(bodyParser)
