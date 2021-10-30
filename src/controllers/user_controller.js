8b11019c78bb2ea6c144e1769a5148f9b3d0a7d7
terminator_79 <aezz120813@gmail.com>
2021-10-30 20:28:49 +0600

    delete test console.logs and test functions

--- src/controllers/user_controller.js
+++ src/controllers/user_controller.js
@@ -7,55 +7,29 @@ class UserController {
     async activate(ctx) {
         const { token } = ctx.params
         const userData = await user_service.activate(token)
-        console.log(userData.refreshToken);
         cookie.create(ctx.res, 'refreshToken', userData.refreshToken, { maxAge: JWT_REFRESH_COOKIE_MS, httpOnly: true })
-        // cookie.create(ctx.res, 'refreshToken', userData.refreshToken, { maxAge: JWT_REFRESH_COOKIE_MS, httpOnly: true })
-        // ctx.cookies.set('refreshToken', userData.refreshToken, { maxAge: JWT_REFRESH_COOKIE_MS, httpOnly: true })
         ctx.body = userData
     }
 
     async login(ctx) {
         const { email, password } = ctx.request.body
         const userData = await user_service.login(email, password)
-        console.log(userData.refreshToken);
         cookie.create(ctx.res, 'refreshToken', userData.refreshToken, { maxAge: JWT_REFRESH_COOKIE_MS, httpOnly: true })
-        // ctx.cookies.set('refreshToken', userData.refreshToken, { maxAge: JWT_REFRESH_COOKIE_MS, httpOnly: true })
         ctx.body = userData
     }
 
     async logout(ctx) {
         const refreshToken = ctx.cookies.get('refreshToken')
         await user_service.logout(refreshToken)
-        console.log(userData.refreshToken);
         cookie.create(ctx.res, 'refreshToken', '', { httpOnly: true })
-        // ctx.cookies.set('refreshToken', "", { httpOnly: true })
         ctx.status = 200
     } 
 
     async refresh(ctx) {
-        console.log(ctx.cookies.get('refreshToken'))
         const refreshToken = ctx.cookies.get('refreshToken')
         const userData = await user_service.refresh(refreshToken)
-        console.log(userData.refreshToken);
         cookie.create(ctx.res, 'refreshToken', userData.refreshToken, { maxAge: JWT_REFRESH_COOKIE_MS, httpOnly: true })
-        // ctx.cookies.set('refreshToken', userData.refreshToken, { maxAge: JWT_REFRESH_COOKIE_MS, httpOnly: true })
         ctx.body = userData
-    }
-
-    get(ctx) {
-        ctx.body = [ 
-            ctx.cookies.get('refreshToken')
-            // cookie.get(ctx.request, 'user')
-        ]
-
-        // ctx.redirect('https://sun9-87.userapi.com/impf/c639728/v639728163/6c2c/eTyC7rSb8bs.jpg?size=1215x2160&quality=96&sign=d45940d06fc3eb1058e5b52f4713b23a&type=album')
-
-        // ctx.body = ctx.cookies.get('refreshToken')
-    }
-
-    post(ctx) {
-        // cookie.create(ctx.res, 'user', ctx.request.body["user"])
-        ctx.status = 200
     }
 }
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,28 +1,14 @@
 const router = require('koa-router')()
-const cookie = require('node-cookie')
 const { User, Token } = require('../database/db')
-const user_service = require('../services/user_service')
 
 const auth_router = require('./auth_router')
 
 router
     .use(auth_router.routes())
+
+    // dev routes
     .get('/users', async ctx => {
         ctx.body = await User.findAll({ include: [Token] })
-    })
-    .get('/tokens', async ctx => {
-        ctx.body = await Token.findAll()
-    })
-    .get('/', ctx => {
-        ctx.body = [ 
-            cookie.get(ctx.request, 'refreshToken'), 
-            cookie.get(ctx.request, 'user')
-        ]
-        // ctx.body = ctx.cookies.get('refreshToken')
-    })
-    .post('/', ctx => {
-        cookie.create(ctx.res, 'user', ctx.request.body["user"])
-        ctx.status = 200
     })
 
 module.exports = router
