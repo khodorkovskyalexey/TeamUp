1304b840aa93dfd0adf09d51a6b40288cc4ae92b
terminator_79 <aezz120813@gmail.com>
2021-10-30 16:58:07 +0600

    try to fix

--- src/controllers/user_controller.js
+++ src/controllers/user_controller.js
@@ -1,27 +1,34 @@
 const { JWT_REFRESH_COOKIE_MS } = require('../configs/jwt_config')
 const user_service = require('../services/user_service')
 
-// const cookie = require('node-cookie')
+const cookie = require('node-cookie')
 
 class UserController {
     async activate(ctx) {
         const { token } = ctx.params
         const userData = await user_service.activate(token)
-        ctx.cookies.set('refreshToken', userData.refreshToken, { maxAge: JWT_REFRESH_COOKIE_MS, httpOnly: true })
+        console.log(userData.refreshToken);
+        cookie.create(ctx.res, 'refreshToken', userData.refreshToken, { maxAge: JWT_REFRESH_COOKIE_MS, httpOnly: true })
+        // cookie.create(ctx.res, 'refreshToken', userData.refreshToken, { maxAge: JWT_REFRESH_COOKIE_MS, httpOnly: true })
+        // ctx.cookies.set('refreshToken', userData.refreshToken, { maxAge: JWT_REFRESH_COOKIE_MS, httpOnly: true })
         ctx.body = userData
     }
 
     async login(ctx) {
         const { email, password } = ctx.request.body
         const userData = await user_service.login(email, password)
-        ctx.cookies.set('refreshToken', userData.refreshToken, { maxAge: JWT_REFRESH_COOKIE_MS, httpOnly: true })
+        console.log(userData.refreshToken);
+        cookie.create(ctx.res, 'refreshToken', userData.refreshToken, { maxAge: JWT_REFRESH_COOKIE_MS, httpOnly: true })
+        // ctx.cookies.set('refreshToken', userData.refreshToken, { maxAge: JWT_REFRESH_COOKIE_MS, httpOnly: true })
         ctx.body = userData
     }
 
     async logout(ctx) {
         const refreshToken = ctx.cookies.get('refreshToken')
         await user_service.logout(refreshToken)
-        ctx.cookies.set('refreshToken', "", { httpOnly: true })
+        console.log(userData.refreshToken);
+        cookie.create(ctx.res, 'refreshToken', '', { httpOnly: true })
+        // ctx.cookies.set('refreshToken', "", { httpOnly: true })
         ctx.status = 200
     } 
 
@@ -29,7 +36,9 @@ class UserController {
         console.log(ctx.cookies.get('refreshToken'))
         const refreshToken = ctx.cookies.get('refreshToken')
         const userData = await user_service.refresh(refreshToken)
-        ctx.cookies.set('refreshToken', userData.refreshToken, { maxAge: JWT_REFRESH_COOKIE_MS, httpOnly: true })
+        console.log(userData.refreshToken);
+        cookie.create(ctx.res, 'refreshToken', userData.refreshToken, { maxAge: JWT_REFRESH_COOKIE_MS, httpOnly: true })
+        // ctx.cookies.set('refreshToken', userData.refreshToken, { maxAge: JWT_REFRESH_COOKIE_MS, httpOnly: true })
         ctx.body = userData
     }
 
