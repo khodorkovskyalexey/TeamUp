4282d5a94b7c1598bb201af4bc07cbab122517df
terminator_79 <aezz120813@gmail.com>
2021-10-30 22:38:09 +0600

    add path='/' in cookie options

--- src/controllers/user_controller.js
+++ src/controllers/user_controller.js
@@ -7,28 +7,28 @@ class UserController {
     async activate(ctx) {
         const { token } = ctx.params
         const userData = await user_service.activate(token)
-        cookie.create(ctx.res, 'refreshToken', userData.refreshToken, { maxAge: JWT_REFRESH_COOKIE_MS, httpOnly: true })
+        cookie.create(ctx.res, 'refreshToken', userData.refreshToken, { maxAge: JWT_REFRESH_COOKIE_MS, httpOnly: true, path: '/' })
         ctx.body = userData
     }
 
     async login(ctx) {
         const { email, password } = ctx.request.body
         const userData = await user_service.login(email, password)
-        cookie.create(ctx.res, 'refreshToken', userData.refreshToken, { maxAge: JWT_REFRESH_COOKIE_MS, httpOnly: true })
+        cookie.create(ctx.res, 'refreshToken', userData.refreshToken, { maxAge: JWT_REFRESH_COOKIE_MS, httpOnly: true, path: '/' })
         ctx.body = userData
     }
 
     async logout(ctx) {
         const refreshToken = ctx.cookies.get('refreshToken')
         await user_service.logout(refreshToken)
-        cookie.create(ctx.res, 'refreshToken', '', { httpOnly: true })
+        cookie.create(ctx.res, 'refreshToken', '', { httpOnly: true, path: '/' })
         ctx.status = 200
     } 
 
     async refresh(ctx) {
         const refreshToken = ctx.cookies.get('refreshToken')
         const userData = await user_service.refresh(refreshToken)
-        cookie.create(ctx.res, 'refreshToken', userData.refreshToken, { maxAge: JWT_REFRESH_COOKIE_MS, httpOnly: true })
+        cookie.create(ctx.res, 'refreshToken', userData.refreshToken, { maxAge: JWT_REFRESH_COOKIE_MS, httpOnly: true, path: '/' })
         ctx.body = userData
     }
 }
