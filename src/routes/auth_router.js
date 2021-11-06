6c4cf359e86dbe3b8e2531c308d2f6b1be9c87b8
terminator_79 <aezz120813@gmail.com>
2021-11-06 23:14:53 +0600

    rename user_service and user_controller to auth_service and auth_controller

--- src/routes/auth_router.js
+++ src/routes/auth_router.js
@@ -1,5 +1,5 @@
 const router = require('koa-router')()
-const user_controller = require('../controllers/user_controller')
+const auth_controller = require('../controllers/auth_controller')
 const email_controller = require('../controllers/email_controller')
 const { body } = require('koa-req-validation')
 
@@ -19,10 +19,10 @@ router.post('/login',
     body("email").isEmail().withMessage("Почта должна быть указана в верном формате").build(),
     body('password').isLength({ min: 8 }).withMessage("Пароль должен содержать не менее 8 символов").build(),
     auth_validate,
-    user_controller.login
+    auth_controller.login
 )
-router.get('/activate/:token', user_controller.activate)
-router.post('/logout', user_controller.logout)
-router.get('/refresh', user_controller.refresh)
+router.get('/activate/:token', auth_controller.activate)
+router.post('/logout', auth_controller.logout)
+router.get('/refresh', auth_controller.refresh)
 
 module.exports = router
